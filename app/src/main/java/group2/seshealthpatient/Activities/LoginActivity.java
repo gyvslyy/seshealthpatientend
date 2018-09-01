9c1ffc576d61ab2052d9164e99a718e8231d2bed
Yueyang Li <gyvslyy@gmail.com>
2018-09-01 23:52:32 +1000

    001

---
+++ app/src/main/java/group2/seshealthpatient/Activities/LoginActivity.java
@@ -0,0 +1,127 @@
+package group2.seshealthpatient.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import group2.seshealthpatient.R;
+
+/**
+ * Class: LoginActivity
+ * Extends: {@link AppCompatActivity}
+ * Author: Carlos Tirado < Carlos.TiradoCorts@uts.edu.au> and YOU!
+ * Description:
+ * <p>
+ * Welcome to the first class in the project. I will be leaving some comments like this through all
+ * the classes I write in order to help you get a hold on the project. Here I took the liberty of
+ * creating an empty Log In activity for you to fill in the details of how your log in is
+ * gonna work. Please, Modify Accordingly!
+ * <p>
+ */
+public class LoginActivity extends AppCompatActivity {
+
+
+    /**
+     * Use the @BindView annotation so Butter Knife can search for that view, and cast it for you
+     * (in this case it will get casted to Edit Text)
+     */
+    @BindView(R.id.usernameET)
+    EditText usernameEditText;
+
+    /**
+     * If you want to know more about Butter Knife, please, see the link I left at the build.gradle
+     * file.
+     */
+    @BindView(R.id.passwordET)
+    EditText passwordEditText;
+
+    /**
+     * It is helpful to create a tag for every activity/fragment. It will be easier to understand
+     * log messages by having different tags on different places.
+     */
+    private static String TAG = "LoginActivity";
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // You need this line on your activity so Butter Knife knows what Activity-View we are referencing
+        ButterKnife.bind(this);
+
+        // A reference to the toolbar, that way we can modify it as we please
+        Toolbar toolbar = findViewById(R.id.login_toolbar);
+        setSupportActionBar(toolbar);
+
+        // Please try to use more String resources (values -> strings.xml) vs hardcoded Strings.
+        setTitle(R.string.login_activity_title);
+
+
+        //Firebase
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    /**
+     * See how Butter Knife also lets us add an on click event by adding this annotation before the
+     * declaration of the function, making our life way easier.
+     */
+    @OnClick(R.id.login_btn)
+    public void LogIn() {
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        // TODO: For now, the login button will simply print on the console the username/password and let you in
+        // TODO: It is up to you guys to implement a proper login system
+
+        // Having a tag, and the name of the function on the console message helps allot in
+        // knowing where the message should appear.
+
+        if(!username.equals("")&& !password.equals("")){
+        Log.d(TAG, "LogIn: username: " + username + " password: " + password);
+        mAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    // Start a new activity
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                }
+                else{
+                Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
+    }
+    else{
+            Toast.makeText(getApplicationContext(), "You did not fill in all the fields.", Toast.LENGTH_SHORT).show();}
+    }
+
+    @OnClick(R.id.Register_Btn)
+    public void Register(){
+    Intent intent1 = new Intent (this, RegisterActivity.class);
+    startActivity(intent1);
+    }
+
+
+}
---
+++ app/src/main/java/group2/seshealthpatient/Activities/RegisterActivity.java
@@ -0,0 +1,119 @@
+package group2.seshealthpatient.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import group2.seshealthpatient.R;
+
+
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private static final String TAG = "RegisterActivity";
+    EditText EmailText, PasswordText, Confirm_passwordText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        EmailText = findViewById(R.id.EmailTextfield);
+        PasswordText = findViewById(R.id.PasswordTextfield);
+        Confirm_passwordText = findViewById(R.id.ConfirmTestfield);
+        findViewById(R.id.CreateBtn).setOnClickListener(this);
+    }
+
+   private void RegisterUser() {
+
+       String Email = EmailText.getText().toString().trim();
+       String Password = PasswordText.getText().toString().trim();
+       String ConfirmPassword = Confirm_passwordText.getText().toString().trim();
+
+       if (Email.isEmpty()) {
+           EmailText.setError("Email is required!");
+           EmailText.requestFocus();
+           return;
+       }
+
+       if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
+           EmailText.setError("Please enter a valid email!");
+           EmailText.requestFocus();
+           return;
+       }
+
+
+       if (Password.isEmpty()) {
+           PasswordText.setError("Password is required!");
+           PasswordText.requestFocus();
+           return;
+       }
+
+       if (Password.length() < 6) {
+           PasswordText.setError("Minimum length of password should be 6");
+           PasswordText.requestFocus();
+           return;
+       }
+
+       if (Password.equals(ConfirmPassword) == false) {
+           Confirm_passwordText.setError("Please enter the same password!");
+           Confirm_passwordText.requestFocus();
+           return;
+       }
+
+       mAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+           @Override
+           public void onComplete(@NonNull Task<AuthResult> task) {
+               if (task.isSuccessful()) {
+                   Toast.makeText(getApplicationContext(), "User registered successfully", Toast.LENGTH_SHORT).show();
+               } else {
+                   if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                       Toast.makeText(getApplicationContext(), "You are already registered", Toast.LENGTH_LONG).show();
+                   } else {
+                       Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                   }
+
+               }
+           }
+       });
+   }
+
+
+
+
+
+
+
+
+    @Override public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.CreateBtn:
+                RegisterUser();
+                break;
+                }
+                }
+
+
+
+
+
+
+
+
+
+        }
